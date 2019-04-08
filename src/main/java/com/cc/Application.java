c860090bd0334a7d3f220e536d410d312bb4d698
anshuiitk <himanshu.kgp@gmail.com>
2019-04-08 07:32:18 +0100

    XX-1

--- src/main/java/com/cc/Application.java
+++ src/main/java/com/cc/Application.java
@@ -1,24 +1,13 @@
 package com.cc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/com/cc/service/impl/CreditCardServiceImpl.java
+++ src/main/java/com/cc/service/impl/CreditCardServiceImpl.java
@@ -1,28 +1,31 @@
 package com.cc.service.impl;
 
+import com.cc.dao.CreditCardDao;
 import com.cc.service.CreditCardService;
 import com.cc.to.CreditCard;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class CreditCardServiceImpl implements CreditCardService {
 
+    private CreditCardDao creditCardDao;
+
     @Override
     public List<CreditCard> getAll() {
-        ArrayList<CreditCard> creditCards = new ArrayList<>();
-        CreditCard creditCard = new CreditCard("AG", "1234 1234 1234 1234", new BigDecimal("5000.00"), new BigDecimal("1000.00"));
-        creditCards.add(creditCard);
-        creditCard = new CreditCard("AG", "1234 1234 1234 5678", new BigDecimal("6000.00"), new BigDecimal("500.00"));
-        creditCards.add(creditCard);
+        List<CreditCard> creditCards = creditCardDao.getAll();
         return creditCards;
     }
 
     @Override
     public boolean add(CreditCard creditCard) {
         return false;
+    }
+
+    @Autowired
+    public void setCreditCardDao(CreditCardDao creditCardDao) {
+        this.creditCardDao = creditCardDao;
     }
 }
